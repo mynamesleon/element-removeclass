c4c26e4889c699a0532b18d3c075d56e619e48f6
mynamesleon <contact@mynamesleon.com>
2020-01-09 20:11:28 +0000

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # element-removeclass
-Tiny zero dependency helper module for removing classes from an element
+
+Tiny zero dependency helper module for removing classes from elements
+
+This emulates jQuery's `removeClass` behaviour: it works for multiple elements, can take a space delimitted String for multiple classes, and even works in awful browsers.
+
+```javascript
+removeClass(elements, 'class-1 class-2 class-3');
+```
+
+## Installation and usage
+
+Grab from NPM
+
+```
+npm install element-removeclass
+```
+
+And import it
+
+```javascript
+import removeClass from 'element-removeclass';
+```
+
+Or grab the minified JavaScript from unpkg
+
+```html
+<script src="https://unpkg.com/element-removeclass"></script>
+```
---
+++ src/element-removeclass.js
@@ -0,0 +1,36 @@
+const REGEX_TRIM = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+const trim = str => (str == null ? '' : (str + '').replace(REGEX_TRIM, ''));
+
+/**
+ * @description remove class(es) from element(s) - support pre `classList`
+ * @param {Element} elem
+ * @param {String} classes - space delimitted class(es) to remove
+ */
+export default function removeClass(elem, classes) {
+    if (elem && typeof elem.length === 'number') {
+        for (let i = 0, l = elem.length; i < l; i += 1) {
+            removeClass(elem[i], classes);
+        }
+        return;
+    }
+
+    // make sure first argument is an element
+    if (!elem || !elem.nodeType) {
+        return;
+    }
+
+    // use current class with spaces each side for string checking
+    const curValue = (elem.getAttribute && elem.getAttribute('class')) || '';
+    let finalValue = ` ${curValue} `;
+
+    // split at spaces for checking multiple classes to remove
+    for (let i = 0, cs = classes.split(' '), l = cs.length; i < l; i += 1) {
+        finalValue = finalValue.replace(` ${cs[i]} `, ' ');
+    }
+
+    // trim final value for html validity
+    finalValue = trim(curValue + finalValue);
+    if (curValue !== finalValue) {
+        elem.setAttribute('class', finalValue);
+    }
+}
