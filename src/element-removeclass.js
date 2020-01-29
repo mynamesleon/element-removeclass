491362b2af1d0335d7d614be41ae8a6cb333da5e
mynamesleon <contact@mynamesleon.com>
2020-01-29 18:05:30 +0000

    correct elem vs array(like) detection

--- src/element-removeclass.js
+++ src/element-removeclass.js
@@ -1,24 +1,7 @@
 const REGEX_TRIM = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
 const trim = str => (str == null ? '' : (str + '').replace(REGEX_TRIM, ''));
 
-/**
- * @description remove class(es) from element(s) - support pre `classList`
- * @param {Element|Element[]} elem
- * @param {String} classes - space delimitted class(es) to remove
- */
-export default function removeClass(elem, classes) {
-    if (elem && typeof elem.length === 'number') {
-        for (let i = 0, l = elem.length; i < l; i += 1) {
-            removeClass(elem[i], classes);
-        }
-        return;
-    }
-
-    // make sure first argument is an element
-    if (!elem || !elem.nodeType) {
-        return;
-    }
-
+function remove(elem, classes) {
     // use current class with spaces each side for string checking
     const curValue = (elem.getAttribute && elem.getAttribute('class')) || '';
     let finalValue = ` ${curValue} `;
@@ -32,5 +15,22 @@ export default function removeClass(elem, classes) {
     finalValue = trim(finalValue);
     if (curValue !== finalValue) {
         elem.setAttribute('class', finalValue);
+    }
+}
+
+/**
+ * @description remove class(es) from element(s) - support pre `classList`
+ * @param {Element|Element[]} elem
+ * @param {String} classes - space delimitted class(es) to remove
+ */
+export default function removeClass(elem, classes) {
+    if (elem && elem.nodeType === 1 && typeof classes === 'string') {
+        // element handling
+        remove(elem, classes);
+    } else if (elem && typeof elem.length === 'number') {
+        // array(like) handling
+        for (let i = 0, l = elem.length; i < l; i += 1) {
+            removeClass(elem[i], classes);
+        }
     }
 }
